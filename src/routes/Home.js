e19940d6944ff1a7696e405c516686938c12fb1b
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-11-10 03:32:15 +0900

    feat:headline

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,11 @@
+import Headline from "../components/Headline";
 import { Component } from "../core";
 
 export default class Home extends Component {
-  
+  render() {
+    const headline = new Headline().el
+
+    this.el.classList.add('container')
+    this.el.append(headline)
+  }
 }
